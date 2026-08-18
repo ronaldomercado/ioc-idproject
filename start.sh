289a78a0cfea69c93f4ab0e6f9f89819aae87eaa
Ronaldo Mercado <ronaldo.mercado@diamond.ac.uk>
2026-08-18 16:33:00 +0200

    add container start script

--- start.sh
+++ start.sh
@@ -1,9 +1,14 @@
+#!/bin/bash
+
+# A launcher for the epics container 
+thisdir=$(realpath $(dirname ${0}))
+
 volumes="
--v /home/tempuser/dev:/workspace
+-v=${thisdir}/..:/workspace
 -v /home/tempuser/dev/ts02k-motion:/epics/ts02k-motion
 "
 opts="
 --net host
 --security-opt=label=type:container_runtime_t
 "
-podman run -dit ${volumes} ${opts}--name=ts02k-motion localhost/ec_test:latest 
+podman run -dit ${volumes} ${opts} --name=ts16k-ioc1 localhost/ioc-idproject:latest 
