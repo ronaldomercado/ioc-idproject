3344ceac5dc57bc8fb36d4c58a8b2edcbb6ad2de
Ronaldo Mercado <ronaldo.mercado@diamond.ac.uk>
2026-08-18 16:35:07 +0200

    modify script to use the released package instead of the local build

--- start.sh
+++ start.sh
@@ -5,10 +5,10 @@ thisdir=$(realpath $(dirname ${0}))
 
 volumes="
 -v=${thisdir}/..:/workspace
--v /home/tempuser/dev/ts02k-motion:/epics/ts02k-motion
 "
+
 opts="
 --net host
 --security-opt=label=type:container_runtime_t
 "
-podman run -dit ${volumes} ${opts} --name=ts16k-ioc1 localhost/ioc-idproject:latest 
+podman run -dit ${volumes} ${opts} --name=ts16k-ioc1 ghcr.io/ronaldomercado/ioc-idproject-developer:0.2
