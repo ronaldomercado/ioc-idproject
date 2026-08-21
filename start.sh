d995e854285ffc3c6938ea9c225fcfb757e2cac6
Ronaldo Mercado <ronaldo.mercado@diamond.ac.uk>
2026-08-21 10:03:24 +0200

    add start script

--- start.sh
+++ start.sh
@@ -11,4 +11,5 @@ opts="
 --net host
 --security-opt=label=type:container_runtime_t
 "
-podman run -dit ${volumes} ${opts} --name=ts16k-ioc1 ghcr.io/ronaldomercado/ioc-idproject-developer:0.2
+podman run -dit ${volumes} ${opts} --name=ts16k-ioc1 ghcr.io/ronaldomercado/ioc-idproject-developer:0.3
+#podman run -dit ${volumes} ${opts} --name=ts16k-ioc1 localhost/k16ws
